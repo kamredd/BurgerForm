9ee76ac42b7dbefe6dfa1947731e92c974b84a10
Admin <Admin@DESKTOP-LL39NJV>
2023-02-07 21:36:20 -0500

    Add project files.

---
+++ BurgerForm/frmBurgers.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmBurgers
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnPrime.Click
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+    End Sub
+End Class
